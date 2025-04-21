24255b8470aafd4590af8b19952d1f09611a8cfa
Kirill Utlik <utlik2004.3@mail.ru>
2025-04-21 23:16:55 +0500

    мэйн страница2

--- src/module/footer/footer.tsx
+++ src/module/footer/footer.tsx
@@ -6,6 +6,32 @@ export default function Footer({ }: Props) {
   
 
   return (
-    <div></div>
+    <div className='w-full flex flex-raw mt-[266px] mb-[50px]'>
+        <div className='w-full flex flex-col'>
+          <img src="/image/logo2.png" alt="" className='w-[266px] pb-[55px]'/>
+          <div className='flex flex-row gap-12'>
+            <img src="/image/tg.png" alt="" />
+            <img src="/image/tt.png" alt="" />
+            <img src="/image/inst.png" alt="" />
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col font-bold mt-[45px]'>
+          <p className='text-[#B8B8B8] text-[35px]'>Ждём вас у нас</p>
+          <p className='text-[#B8B8B8] text-[35px]'>с 10 до 22</p>
+        </div>
+
+        <div className='w-full flex flex-col justify-between gap-2 font-bold'>
+          <p className='text-[#B8B8B8] text-[35px]'>Главная</p>
+          <p className='text-[#B8B8B8] text-[35px]'>Меню</p>
+          <p className='text-[#B8B8B8] text-[35px]'>Доставка</p>
+        </div>
+
+        <div className='w-full flex flex-col justify-between gap-2 font-bold'>
+          <p className='text-[#B8B8B8] text-[35px]'>Сайт франшизы</p>
+          <p className='text-[#B8B8B8] text-[35px]'>8 (905)231 38 55</p>
+          <p className='text-[#B8B8B8] text-[35px]'>Разработчики блум</p>
+        </div>
+    </div>
   );
 }
--- src/module/location/location.tsx
+++ src/module/location/location.tsx
@@ -3,22 +3,27 @@ import { useState } from 'react';
 type Props = {}
 
 export default function Location({ }: Props) {
-  
-
   return (
-    <div className='w-full'>
-        <div>
-            <p>Удобное расположение в центре Екатеринбурга в</p>
-            <p>вашем любимом торговом центре Гринвич!</p>
-        </div>
+    <div className='w-full max-w-[1440px] mx-auto px-4 py-8'>
 
-        <p>ул. 8 марта, 46; 3 этаж; фуд-маркет Estory</p>
+    <div className='oval1'/>
+    <div className='oval2'/>
 
-        <div className='w-full flex items-center justify-between relative '>
-            <img src="/image/moti.png" alt="" className=''/>
-            <img src="/image/map.png" alt="" className=''/>
-            <img src="image/cotik.png" alt="" className=''/>
-        </div>
+      {/* Текстовый блок */}
+      <div className='mb-8'>
+        <p className='text-2xl font-bold text-center mb-2 text-[#7EDAFF] text-[50px] mt-[115px]'>Удобное расположение в центре Екатеринбурга в</p>
+        <p className='text-2xl font-bold text-center text-[#7EDAFF] text-[50px]'>вашем любимом торговом центре Гринвич!</p>
+      </div>
+
+      {/* Адрес */}
+      <p className='text-xl text-center mt-[25px] mb-12 text-[#FD744C] text-[40px]'>ул. 8 марта, 46; 3 этаж; фуд-маркет Estory</p>
+
+      {/* Галерея изображений */}
+      <div className='w-full flex items-center justify-between relative'>
+        <img src="/image/moti.png" alt="" className='moti relative m-0'/>
+        <img src="/image/map.png" alt="" className='ml-[315px]'/>
+        <img src="/image/cotik.png" alt="" className='kotik relative m-0'/>
+      </div>
     </div>
   );
 }
